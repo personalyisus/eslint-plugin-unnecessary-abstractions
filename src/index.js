3a3900855d80583f6d30fb26733f229a18e606d6
Jesús Marín <marinalvjesus@gmail.com>
2024-07-25 22:48:52 -0400

    feat: bumping version to 0.1.4

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const noUnnecessaryTernaryWrappers = require("./rules/no-ternary-wrappers.js");
 module.exports = {
   meta: {
     name: "unnecessary-abstractions",
-    version: "0.1.3",
+    version: "0.1.4",
   },
   rules: {
     "no-ternary-wrappers": noUnnecessaryTernaryWrappers,
