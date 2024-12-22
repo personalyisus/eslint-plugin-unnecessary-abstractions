4dccf8370c9bfb1508157e450020950622888f49
Jesús Marín <marinalvjesus@gmail.com>
2024-12-21 22:27:43 -0300

    chore: moving methods that depend into create()

--- src/rules/no-ternary-wrappers.js
+++ src/rules/no-ternary-wrappers.js
@@ -1,44 +1,3 @@
-const arrowFunctionCheck = (node, context) => {
-  // If the only child of the arrow function
-  // is a ternary expression, report an error
-  if (node.body.type === "ConditionalExpression") {
-    context.report({
-      node,
-      message:
-        "This is an unnecessary abstraction. Prefer using the ternary expression directly instead of wrapping it in a function.",
-    });
-  }
-};
-
-const blockStatementCheck = (node, context) => {
-  const applicableParentTypes = [
-    "FunctionExpression",
-    "ArrowFunctionExpression",
-    "MethodDefinition",
-    "FunctionDeclaration",
-  ];
-
-  // If the node.parent.type isn't one that
-  // interests us, we don't need to do anything
-  if (!applicableParentTypes.includes(node.parent.type)) {
-    return;
-  }
-
-  // If the only child of the function
-  // is a ternary expression, report an error
-  if (
-    node.body.length === 1 &&
-    node.body[0].type === "ReturnStatement" &&
-    node.body[0].argument.type === "ConditionalExpression"
-  ) {
-    context.report({
-      node,
-      message:
-        "This is an unnecessary abstraction. Prefer using the ternary expression directly instead of wrapping it in a function.",
-    });
-  }
-};
-
 module.exports = {
   meta: {
     type: "suggestion",
@@ -49,13 +8,49 @@ module.exports = {
     },
   },
   create(context) {
+    const blockStatementCheck = (node) => {
+      const applicableParentTypes = [
+        "FunctionExpression",
+        "ArrowFunctionExpression",
+        "MethodDefinition",
+        "FunctionDeclaration",
+      ];
+
+      // If the node.parent.type isn't one that
+      // interests us, we don't need to do anything
+      if (!applicableParentTypes.includes(node.parent.type)) {
+        return;
+      }
+
+      // If the only child of the function
+      // is a ternary expression, report an error
+      if (
+        node.body.length === 1 &&
+        node.body[0].type === "ReturnStatement" &&
+        node.body[0].argument.type === "ConditionalExpression"
+      ) {
+        context.report({
+          node,
+          message:
+            "This is an unnecessary abstraction. Prefer using the ternary expression directly instead of wrapping it in a function.",
+        });
+      }
+    };
+
+    const arrowFunctionCheck = (node) => {
+      // If the only child of the arrow function
+      // is a ternary expression, report an error
+      if (node.body.type === "ConditionalExpression") {
+        context.report({
+          node,
+          message:
+            "This is an unnecessary abstraction. Prefer using the ternary expression directly instead of wrapping it in a function.",
+        });
+      }
+    };
     return {
-      BlockStatement(node) {
-        blockStatementCheck(node, context);
-      },
-      ArrowFunctionExpression(node) {
-        arrowFunctionCheck(node, context);
-      },
+      BlockStatement: blockStatementCheck,
+      ArrowFunctionExpression: arrowFunctionCheck,
     };
   },
 };
